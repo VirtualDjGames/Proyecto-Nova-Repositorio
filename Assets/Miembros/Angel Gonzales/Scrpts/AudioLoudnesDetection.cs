c2f95463e2c221f9b7bb19d68951908a265f0f78
Angelz115 <66693671+Angelz115@users.noreply.github.com>
2023-05-16 12:41:08 -0400

    hacer que el microfono funcione en el telefono

--- Assets/Miembros/Angel Gonzales/Scrpts/AudioLoudnesDetection.cs
+++ Assets/Miembros/Angel Gonzales/Scrpts/AudioLoudnesDetection.cs
@@ -1,33 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioLoudnesDetection : MonoBehaviour
 {
     public int sampleWindow = 64;
     private AudioClip microphoneClip;
+    private string microphoneName;
     // Start is called before the first frame update
     void Start()
     {
+        microphoneName = Microphone.devices.Length > 0 ? Microphone.devices[0] : null;
+
+        if (microphoneName == null)
+        {
+            Debug.LogError("No microphone devices found.");
+            return;
+        }
+
         MicrophoneToAudioClip();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     public void MicrophoneToAudioClip() 
     {
-        //Get the microphone in device list
-        string microphoneName = Microphone.devices[0];
+        
         microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
 
     }
 
     public float GetLoudnessFromMicrophone() 
     {
+        if (microphoneClip == null)
+        {
+            Debug.LogError("Microphone clip is null.");
+            return 0f;
+        }
+
         return GetLoudnesFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
     }
 
